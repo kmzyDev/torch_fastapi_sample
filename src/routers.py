912f1cab3a64f4cae6a2771efb2f21760fa30512
fuga <hoge>
2025-03-29 23:14:29 +0900

    disable async

--- src/routers.py
+++ src/routers.py
@@ -6,5 +6,5 @@ from core import hoge
 router = APIRouter()
 
 @router.post("/hoge")
-async def hoge_endpoint(request: InputParams) -> str:
+def hoge_endpoint(request: InputParams) -> str:
     return hoge(request)
